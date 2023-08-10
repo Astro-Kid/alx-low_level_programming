c198113b8fa52af8cec1a93cdca20280f6c0098f
Astro-Kid <bupechiyana15@gmail.com>
2023-08-10 16:07:05 -0700

    gcc -Wall -Werror -Wextra -pedantic -std=gnu89 5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - Print Hello , printf
+ *
+ * Return: 0 when true
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
