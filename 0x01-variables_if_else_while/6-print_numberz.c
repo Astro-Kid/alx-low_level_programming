aaf2383c80fecdfb9912603f12bf48cd25ad98e1
Astro-Kid <bupechiyana15@gmail.com>
2023-08-14 13:40:46 -0700

    Arrange

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers of base 10 from 0
+ * Return: true to output 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
