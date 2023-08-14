aa8913d611c00c1fa0fa3a571b1f31bc1c2f4d00
Astro-Kid <bupechiyana15@gmail.com>
2023-08-14 12:38:19 -0700

    Lowercases

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints lowercase alphabet
+ * Return: If true the output 0
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'e' && ch != 'q')
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
