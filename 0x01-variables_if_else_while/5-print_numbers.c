3d1bbcf6d40b20c185045bb7c0270b28ce4fa917
Astro-Kid <bupechiyana15@gmail.com>
2023-08-14 13:21:40 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - base ten in new line format from 0
+ * Return: true and output 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf("\n");
+	return (0);
+}
