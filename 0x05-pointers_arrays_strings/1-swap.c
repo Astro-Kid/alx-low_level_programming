e722465b3bf01805087e0e1e5135187e7654cf3e
Astro-Kid <bupechiyana15@gmail.com>
2023-08-20 21:06:34 -0700

    1-swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - take in two ints and swaps them
+ * @author Bupe
+ * @a: swap and store address of b
+ * @b: swap and store address of a
+ * Return: 0
+ */
+
+void swap_int(int *a, int *b)
+{
+	int change;
+	change = *b;
+	*b = *a;
+	*a = change;
+}
