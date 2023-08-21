219b56c274a484ca37279420cb1df92246938c36
Astro-Kid <bupechiyana15@gmail.com>
2023-08-21 16:05:36 -0700

    6-puts2.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - print string
+ * @str: reference
+ * return: 0
+ */
+
+void puts2(char *str)
+{
+	int z = 0;
+
+	while (str[z] != '\0')
+	{
+		if (z % 2 == 0)
+		{
+			_putchar(str[z]);
+		}
+		z++;
+	}
+	_putchar('\n');
+}
