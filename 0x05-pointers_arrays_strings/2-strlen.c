4b7f0b55cc6e345243b681be9340d007d98b9edf
Astro-Kid <bupechiyana15@gmail.com>
2023-08-21 13:03:43 -0700

    LL

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - checks the length of string
+ * @s: -string to be checked
+ * Return: The length of the string
+ */
+int _strlen(char *s)
+{
+	int string_length = 0;
+
+	while (s[string_length])
+		string_length++;
+	return (string_length);
+}
