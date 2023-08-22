e51d62fa853e2d6ffc6f033d21b7ec9f670efb88
Astro-Kid <bupechiyana15@gmail.com>
2023-08-21 19:54:21 -0700

    /9-strcpy

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies the str by src
+ * @dest: char type string
+ * @src: char type string
+ * Description: copy the str ptd by  the ptr 'src'
+ * ptr to 'dest'
+ * Return: 'dest'
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int z = -1;
+
+	do {
+		z++;
+		dest[z] = src[z];
+	}
+	while (src[z] != '\0');
+
+	return (dest);
+}
