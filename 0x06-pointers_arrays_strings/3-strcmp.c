eb7d49bd9a488772819b744804442af760929c97
Astro-Kid <bupechiyana15@gmail.com>
2023-08-24 14:30:43 -0700

    ./3-strcmp

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strncmp - string compair
+ * @s1: input val
+ * @s2: input val
+ * Return: s1[i] - s2[i]
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '\0')
+	{
+		if (s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
+}
