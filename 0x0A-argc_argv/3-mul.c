09e097bdd93351f0f792f930a53de66515b4d94b
Astro-Kid <bupechiyyana15@gmail.com>
2023-11-19 13:50:40 -0800

    3-mul.c3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,15 +12,15 @@ int main(int argc, char **argv)
 {
 	int x, y;
 
-	if(argc < 3)
+	if (argc < 3)
 	{
 		printf("Error\n");
 			return (1);
 	}
 
-	x = atoi (argv [1]);
-	y = atoi (argv[2]);
-	printf("%d\n", x = y);
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
 
 	return (0);
 }
