e8bbfc5419c453724ec0857bbd46147f956b40fc
Astro-Kid <bupechiyyana15@gmail.com>
2023-11-22 15:36:24 -0800

    100-change.c

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -29,7 +29,7 @@ int main(int argc, char **argv)
 		{
 			for (i = 0; i < sizeof(cents[i]); i++)
 			{
-				if (total >+ cents[i])
+				if (total >= cents[i])
 				{
 					count += total / cents[i];
 					total = total % cents[i];
